a881bd28b32a1a6a15ec23b090d65b8b254aceb4
konsumschaf <github@konsumschaf.de>
2021-05-04 22:57:49 +0200

    Fix brightness and rounding error

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -1446,11 +1446,11 @@ void GuiMenu::openSystemSettings_batocera()
 	int brighness;
 	if (ApiSystem::getInstance()->getBrighness(brighness))
 	{
-		auto brightnessComponent = std::make_shared<SliderComponent>(mWindow, 5.f, 100.f, 5.f, "%");
+		auto brightnessComponent = std::make_shared<SliderComponent>(mWindow, 1.f, 100.f, 1.f, "%");
 		brightnessComponent->setValue(brighness);
 		brightnessComponent->setOnValueChanged([](const float &newVal)
 		{
-			ApiSystem::getInstance()->setBrighness((int)Math::round(newVal));
+			ApiSystem::getInstance()->setBrighness((int)Math::round(newVal+0.5));
 		});
 
 		s->addWithLabel(_("BRIGHTNESS"), brightnessComponent);
