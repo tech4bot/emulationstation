d0fa47d8bb8168ddccb1c2d5014b0caaed3438c3
dhwz <dhwz@gmx.net>
2021-09-29 18:32:53 +0200

    fix

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -1482,7 +1482,7 @@ void GuiMenu::openSystemSettings_batocera()
 	//std::vector<std::string> availableAudio = ApiSystem::getInstance()->getAvailableAudioOutputDevices();
 	//std::string selectedAudio = ApiSystem::getInstance()->getCurrentAudioOutputDevice();
 	//if (selectedAudio.empty())
-		selectedAudio = "auto";
+	//	selectedAudio = "auto";
 
 	//if (SystemConf::getInstance()->get("system.es.menu") != "bartop")
 	//{
