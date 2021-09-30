75e75d67942e1cb988402bbc234b0609e3ec56cf
dhwz <dhwz@gmx.net>
2021-09-30 16:56:45 +0200

    move volumebar y position

--- es-core/src/components/VolumeInfoComponent.cpp
+++ es-core/src/components/VolumeInfoComponent.cpp
@@ -56,7 +56,7 @@ VolumeInfoComponent::VolumeInfoComponent(Window* window, bool actionLine)
 
 	// FCA TopLeft
 	float posX = Renderer::getScreenWidth() * 0.02f;
-	float posY = Renderer::getScreenHeight() * 0.04f;
+	float posY = Renderer::getScreenHeight() * 0.13f;
 
 	setPosition(posX, posY, 0);
 	setOpacity(BASEOPACITY);
